9fabc1003166a9f335a9e7aa17c994810c5d3d9b
CleanSolLLC <mamark90@gmail.com>
2020-02-20 15:31:01 -0500

    migrations completed

--- db/migrate/20200220201952_create_join_table.rb
+++ db/migrate/20200220201952_create_join_table.rb
@@ -1,5 +1,5 @@
 class CreateJoinTable < ActiveRecord::Migration[6.0]
   def change
-  	create_join_table :users, :tasks table_name: :user_tasks
+  	create_join_table :users, :tasks, table_name: :user_tasks
   end
 end
---
+++ db/schema.rb
@@ -0,0 +1,52 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_02_20_201952) do
+
+  create_table "chores", force: :cascade do |t|
+    t.string "type"
+    t.integer "user_id", null: false
+    t.integer "task_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["task_id"], name: "index_chores_on_task_id"
+    t.index ["user_id"], name: "index_chores_on_user_id"
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.boolean "completed"
+    t.datetime "date_completed"
+    t.string "task_name"
+    t.integer "chore_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["chore_id"], name: "index_tasks_on_chore_id"
+  end
+
+  create_table "user_tasks", id: false, force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "task_id", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "user_name"
+    t.string "email"
+    t.string "password_digest"
+    t.binary "avatar"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "chores", "tasks"
+  add_foreign_key "chores", "users"
+  add_foreign_key "tasks", "chores"
+end
