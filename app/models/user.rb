0a70e74cb6a955e53de4247893b9fa75f57d97a6
CleanSolLLC <mamark90@gmail.com>
2020-02-28 20:45:06 -0500

    updated spec.md file

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,13 +16,14 @@ def self.new_with_session(params, session)
 super.tap do |user|
 
     if data = session['devise.facebook_data'] && session['devise.facebook_data']['extra']['raw_info']
-      user.email = data[“email”] if user.email.blank?
+      user.email = data['email'] if user.email.blank?
     end
   end
 end
 
 def self.from_omniauth(auth)
   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    byebug
     user.email = auth.info.email
     user.password = Devise.friendly_token[0, 20]
     user.username = auth.info.name   # assuming the user model has a name
@@ -32,5 +33,11 @@ def self.from_omniauth(auth)
     # user.skip_confirmation!
   end
 end
+
+#def self.persisted_as_facebook_user?(auth)
+  #this method returns true or false if the the user is persisted in the database with  
+
+
+
 end
  
