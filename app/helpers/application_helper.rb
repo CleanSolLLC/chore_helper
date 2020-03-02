35b82217527a7488b9ecd9af92d75ed9841ff3d6
CleanSolLLC <mamark90@gmail.com>
2020-03-02 12:49:33 -0500

    develop index view to show chores and tasks

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+	def user_login_time
+	  time = Time.now.strftime('%A %B %d, %G %r')
+	end
+
+	def user_last_login_time(currnt_user)
+	  current_user.last_sign_in_at.in_time_zone("Eastern Time (US & Canada)").strftime('%A %B %d, %G %r')
+	end
 end
